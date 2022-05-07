97d5af1f7ce786094904e4b8544367cd710552da
YasithCB <yasith.cb2001@gmail.com>
2022-05-07 11:48:26 +0530

    OrderDAO and OrderDetailsDAO classes created

---
+++ Layered_Architecture/src/dao/OrderDAOImpl.java
@@ -0,0 +1,43 @@
+package dao;
+
+import model.OrderDTO;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class OrderDAOImpl implements CrudDAO<OrderDTO, String>{
+    @Override
+    public ArrayList<OrderDTO> getAll() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean insert(OrderDTO dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(OrderDTO dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean exists(String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public String generateNewId() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public OrderDTO Search(String s) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+}
