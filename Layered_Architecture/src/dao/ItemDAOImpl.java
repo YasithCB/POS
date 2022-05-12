b4bc09e1122788a5102bfb3878d256f6dd9c2ccd
YasithCB <yasith.cb2001@gmail.com>
2022-05-12 13:13:21 +0530

    Make new layer of DAO Interfaces to implemet unique methods of each table

--- Layered_Architecture/src/dao/ItemDAOImpl.java
+++ Layered_Architecture/src/dao/ItemDAOImpl.java
@@ -1,13 +1,11 @@
 package dao;
 
-import db.DBConnection;
-import model.CustomerDTO;
 import model.ItemDTO;
 
 import java.sql.*;
 import java.util.ArrayList;
 
-public class ItemDAOImpl implements CrudDAO<ItemDTO, String>{
+public class ItemDAOImpl implements ItemDAO {
     @Override
     public ArrayList<ItemDTO> getAll() throws SQLException, ClassNotFoundException {
         ResultSet rs = SQLUtil.executeQuery("SELECT * FROM item");
